d6acaa6b277f5d1f6e716e00112a717e37ddaaa5
karlartur <89841872+karlartur@users.noreply.github.com>
2021-09-08 09:59:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HowOldAreYou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsub kasutaja vanust
+            //programm arvutab kasutaja sünniaastat
+            //programm kuvab kasutaja sünniaastat konsoolis
+
+            Console.WriteLine("Kui vana sa oled?");
+
+            //convert string from ReadLine() to int
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            int YearOfBirth = 2021 - Age;
+
+                    }
+    }
+}
